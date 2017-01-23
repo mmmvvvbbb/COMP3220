4220645508d7126222c73869e5f85abb1ab4f0c6
Matt Bonsall <mvb0005@auburn.edu>
2017-01-22 18:48:49 -0600

    Given code

--- RubyProgrammingAssignment1.rb
+++ RubyProgrammingAssignment1.rb
@@ -1,1 +1,49 @@
-puts "Hello World"
+class Scanner
+def initialize(filename)
+	@f = File.open(filename, 'r:utf-8')
+	if (! @f.eof?)
+		@c = @f.getc()
+	else
+		@c = "eof"
+		@f.close()
+	end
+end
+
+def nextCh()
+	if (! @f.eof?)
+		@c = @f.getc()
+	else
+		@c = "eof"
+		@f.close()
+	end
+	return @c
+end
+
+def nextToken()
+	if @c == "eof" 
+		tok = Token.new(Token::EOF,"eof") 
+	elsif (whitespace?(@c)) 
+		str ="" 
+		while whitespace?(@c) 
+			str += @c 
+			nextCh()
+		end 
+		tok = Token.new(Token::WS,str) 
+	elsif 
+		nextCh() 
+		# don't want to give back nil token!
+		tok = Token.new("unknown","unknown") 
+	end 
+	#more needed here...
+	return tok
+end
+end
+def letter?(lookAhead)  
+	lookAhead =~ /[a-z]|[A-Z]/ 
+end 
+def numeric?(lookAhead)
+   lookAhead =~ /^(\d)+$/ 
+end 
+def whitespace?(lookAhead)
+   lookAhead =~ /^(\s)+$/ 
+end 
